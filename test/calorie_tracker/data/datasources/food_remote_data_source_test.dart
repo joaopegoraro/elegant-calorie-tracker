1d62d8ae2e13f49f71e029c2892960e0df23b054
hahmraro <hahmraro@tuta.io>
2021-05-16 12:39:32 -0300

    .

--- test/calorie_tracker/data/datasources/food_remote_data_source_test.dart
+++ test/calorie_tracker/data/datasources/food_remote_data_source_test.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:elegant_calorie_tracker/core/error/exception.dart';
-import 'package:elegant_calorie_tracker/core/utils/production_api.dart';
+import 'package:elegant_calorie_tracker/core/utils/api.dart';
 import 'package:elegant_calorie_tracker/calorie_tracker/data/datasources/food_remote_data_source.dart';
 import 'package:elegant_calorie_tracker/calorie_tracker/data/food_model.dart';
 import 'package:flutter_test/flutter_test.dart';
